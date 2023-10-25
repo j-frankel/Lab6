34bbd624355051321f10a3b1b4e6d2fcc3ffb80a
j-frankel <jacob8frankel@gmail.com>
2023-10-25 14:51:49 -0400

    jacob

--- encode.py
+++ encode.py
@@ -4,31 +4,46 @@ Jacob F & Alex F
 
 '''
 
-def main():
-    print("Menu")
-    print("------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
+def menu(): 
+    user_choice = None
+    print("Menu",
+   "------------",
+   "1. Encode",
+   "2. Decode",
+   "3. Quit", sep='\n')
     user_choice = int(input("\nPlease enter an option: "))
+    return user_choice
 
-    while user_choice != 3:
-        try:
-            if user_choice == 1:
-                input_string_pass = input("Please enter your password to encode: ")
-                list_pass = []
-                for i in input_string_pass:
-                    list_pass.append(i)
-                encoded_list = []
-                for i in list_pass:
-                    i = int(i)
-                    i += 3
-                    encoded_list.append(i)
-                encoded = ''.join(encoded_list)
-                print("Your password has been encoded and stored!")
-                print(encoded)
-        except:
-            print("Error")
+def encode():
+            
+    try:
+        input_string_pass = input("Please enter your password to encode: ")
+        list_pass = []
+
+        for i in input_string_pass:
+            list_pass.append(i)
+        encoded_list = []
+
+        for i in list_pass:
+            i = int(i)
+            i += 3
+            i = str(i)
+            encoded_list.append(i)
+        encoded = ''.join(encoded_list)
+        print("Your password has been encoded and stored!")
+        return encoded
+    except:
+        print("Error")
+
+def main():
+
+    
+    while  != 3:
+        
+        if 
+
+
+    
 
 
 if __name__ == '__main__':
