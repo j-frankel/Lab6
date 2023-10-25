b56c989d8ed9a0f258b2b61889734df6b2116c9f
alexfisher03 <fisherr.alexander@gmail.com>
2023-10-25 14:06:10 -0400

    from alex f

--- encode.py
+++ encode.py
@@ -10,7 +10,22 @@ def main():
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-    print("\nPlease enter an option: ")
+    user_choice = int(input("\nPlease enter an option: "))
+
+    while user_choice != 3:
+        try:
+            if user_choice == 1:
+                list_pass = input("Please enter your password to encode: ")
+                list_pass = list_pass.split()
+                for i in list_pass:
+                    i = int(i)
+                    i += 3
+                    return i
+                encoded = ''.join(list_pass)
+                print("Your password has been encoded and stored!")
+                print(encoded)
+        except:
+            print("Error")
 
 
 if __name__ == '__main__':
